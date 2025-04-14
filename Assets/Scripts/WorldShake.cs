19d0840a10f2bf5db119dcecdd2ed6280f52894f
QGShift <devon.haynes@comcast.net>
2025-04-14 16:50:17 -0400

    Camera Shake added
    Map shake script added to environment parent object.  Use activate boolean to begin/end shake

---
+++ Assets/Scripts/WorldShake.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldShake : MonoBehaviour
+{
+    public GameObject environment;
+    public float magnitude;
+
+    [SerializeField] private float time;
+
+    public bool activate;
+    
+    void Update(){
+        if(activate){
+            Debug.Log("Shaking");
+            StartCoroutine(ShakeEnvironment(environment.transform, time));
+        }
+    }
+
+    IEnumerator ShakeEnvironment(Transform target, float duration)
+    {
+    Vector3 originalPos = target.localPosition;
+    float elapsed = 0f;
+
+    while (elapsed < duration)
+    {
+        float x = Random.Range(-1f, 1f) * magnitude;
+        float y = Random.Range(-1f, 1f) * magnitude;
+
+        target.localPosition = originalPos + new Vector3(x, y, 0);
+        elapsed += Time.deltaTime;
+
+        yield return null;
+    }
+
+    target.localPosition = originalPos;
+    }
+
+}
